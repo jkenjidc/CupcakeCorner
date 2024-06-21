64dc017ad574964e2e94c5f9d961118b72f3c23d
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-06-21 13:23:17 -0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kenji Dela Cruz on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
